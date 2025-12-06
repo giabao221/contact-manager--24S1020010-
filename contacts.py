411f2f95742bcce3ec676fc942daa4c8a33b7f0c
Gia <baobaoofficial2005@gmail.com>
2025-12-06 13:43:02 +0700

    Feature: view_contacts function

--- contacts.py
+++ contacts.py
@@ -10,3 +10,10 @@ def add_contact():
     phone = input("Nhập số điện thoại: ")
     phonebook.append({'name': name, 'phone': phone})
     print("Đã thêm liên hệ.")
+
+def view_contacts():
+    if not phonebook:
+        print("Danh bạ trống.")
+    else:
+        for i, contact in enumerate(phonebook, start=1):
+            print(f"{i}. {contact['name']} - {contact['phone']}")
